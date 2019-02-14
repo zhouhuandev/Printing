32177546bdc866cdfec32cc89cd4ef0aa69d1dc8
ZHOUHUAN_HOME <hz_zhouhuan@qq.com>
2019-02-14 15:13:19 +0800

    主页控制器

---
+++ printing-web-admin/src/main/java/com/yiying/printing/web/admin/web/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.yiying.printing.web.admin.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 首页控制器
+ *
+ * @Description: //TODO
+ * @Author hz_zhouhuan
+ * @Version 1.0.0
+ * @Date 2019/1/26 23:11
+ */
+@Controller
+public class MainController {
+    /**
+     * 跳转首页
+     *
+     * @return
+     */
+    @RequestMapping(value = "main", method = RequestMethod.GET)
+    public String main() {
+        return "main";
+    }
+}
