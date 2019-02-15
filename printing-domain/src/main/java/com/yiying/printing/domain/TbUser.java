675fc5f60218a789e540d2ff7f7529a887545425
ZHOUHUAN_HOME <hz_zhouhuan@qq.com>
2019-02-15 14:51:02 +0800

    更新用户类

--- printing-domain/src/main/java/com/yiying/printing/domain/TbUser.java
+++ printing-domain/src/main/java/com/yiying/printing/domain/TbUser.java
@@ -16,11 +16,11 @@ import javax.validation.constraints.Pattern;
  */
 @Data
 public class TbUser extends BaseEntity {
-    @Length(min = 6, max = 20, message = "姓名长度必须介于 6 和 20 之间")
+    @Length(min = 3, max = 20, message = "姓名长度必须介于 3 和 20 之间")
     private String username;
     //封装数据返回页面时排除password
     @JsonIgnore
-    @Length(min = 6, max = 20, message = "密码长度必须介于 6 和 20 之间")
+    @Length(min = 4, max = 20, message = "密码长度必须介于 4 和 20 之间")
     private String password;
     @Pattern(regexp = RegexpUtils.PHONE, message = "手机号格式不正确")
     private String phone;
