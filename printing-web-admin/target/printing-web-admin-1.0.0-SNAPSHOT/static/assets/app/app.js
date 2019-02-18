2603fd7117ad664a3b1a5e0c06188caa23801ea8
ZHOUHUAN_HOME <hz_zhouhuan@qq.com>
2019-02-18 19:43:23 +0800

    更新了静态资源

--- printing-web-admin/target/printing-web-admin-1.0.0-SNAPSHOT/static/assets/app/app.js
+++ printing-web-admin/target/printing-web-admin-1.0.0-SNAPSHOT/static/assets/app/app.js
@@ -335,31 +335,32 @@ var App = function () {
 
     /**
      * 初始化 bootstrap-fileinput
-     * @param id 初始化的 <input> 标签 id
+     * @param inputId 初始化的 <input> 标签 id
      * @param uploadUrl 上传的路径
-     * @param deleteUrl 删除的路径
      */
-    var handlerInitFileInput = function (id, uploadUrl) {
+    var handlerInitFileInput = function (inputId, uploadUrl) {
         // printing 打印表单上传文件变量
         var _fileNamesArray = new Array();//原文件名数组
         var _fileUrlNamesArray = new Array();//链接地址数组
 
-        $(id).fileinput({
+        $(inputId).fileinput({
             theme: 'explorer-fas', //更改文件载上传框中的样式
             language: 'zh', //更改语言,需要引入语言包zh.js
             uploadUrl: uploadUrl, //上传文件路径
             uploadAsync: false, //false 同步上传，后台用数组接收，true 异步上传，每次上传一个file,会调用多次接口
             allowedFileExtensions: ['docx', 'doc', 'xlsx', 'xls', 'pptx', 'ppt', 'txt'], //接收的文件后缀，如['jpg', 'gif', 'png','docx', 'doc', 'xlsx','xls','pptx','ppt','txt'],不填将不限制上传文件后缀类型
-            maxFileCount: 3, //表示允许同时上传的最大文件个数
+            maxFileCount: 1, //表示允许同时上传的最大文件个数
+            maxFileSize: 1024 * 10,//单位为kb，如果为0表示不限制文件大小,此处限制 1024 * 10 kb = 10 Mb
             dropZoneEnabled: false, //是否显示拖拽区域
             fileActionSettings:{ //设置预览图片的显示样式
-                showUpload: false,
-                showRemove: false,
+                showUpload: false,//关闭预览图的单独上传
+                showRemove: false,//关闭预览图的单独移除
             }
         }).on('filebatchuploadsuccess', function (event, data, id, index) {//批量上传成功文件后的回调函数
             // console.log(data);//后台返回的数据信息在 data.response 里面
-            _fileNamesArray = data.response.fileNames;
-            _fileUrlNamesArray = data.response.fileUrlNames;
+            _fileNamesArray = data.response.fileNames;//获取后台返回的原文件名，数组形式
+            _fileUrlNamesArray = data.response.fileUrlNames;//获取后台返回的全路径，数组形式
+            //赋值给前端的隐藏域
             $("#filename").val(ArrayToString(_fileNamesArray));
             $("#url").val(ArrayToString(_fileUrlNamesArray));
         }).on("fileclear",function(event, data, msg){//点击浏览框右上角X 清空文件前响应事件
@@ -371,10 +372,12 @@ var App = function () {
             var fileUrlNames = ArrayToString(_fileUrlNamesArray);
             //移除文件
             handlerRemoveFile(fileUrlNames);
+            //前端的隐藏域清空
             $("#url").val("");
             $("#filename").val("");
         });
     };
+
     /**
      * 移除函数
      * @param param 移除的地址参数
@@ -494,8 +497,8 @@ var App = function () {
          * @param uploadUrl
          * @param deleteUrl
          */
-        initFileInput: function (id, uploadUrl) {
-            handlerInitFileInput(id, uploadUrl);
+        initFileInput: function (inputId, uploadUrl) {
+            handlerInitFileInput(inputId, uploadUrl);
         },
         /**
          * 移除函数
